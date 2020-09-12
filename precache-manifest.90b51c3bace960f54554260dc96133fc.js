066df0dcdf0f10cd8a132eb7cbac9d4f5c694200
sinha104 <sinha104@umn.edu>
2020-09-12 18:10:58 +0530

    add gh-pages:

---
+++ precache-manifest.90b51c3bace960f54554260dc96133fc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5f3603495f3fed844a153f421ed9882",
+    "url": "/index.html"
+  },
+  {
+    "revision": "212d4d550bd4dbc56140",
+    "url": "/static/css/main.0cb0b71d.chunk.css"
+  },
+  {
+    "revision": "ca62f537a8e12af85851",
+    "url": "/static/js/2.fb9828b0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fb9828b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "212d4d550bd4dbc56140",
+    "url": "/static/js/main.bbbbf7ce.chunk.js"
+  },
+  {
+    "revision": "48450bd7934abbba6876",
+    "url": "/static/js/runtime-main.cf0881eb.js"
+  },
+  {
+    "revision": "c22650c3aa49ae1b5c1ee1ae4b312792",
+    "url": "/static/media/logo_temp_1.c22650c3.png"
+  }
+]);
