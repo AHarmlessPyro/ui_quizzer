bf84301363f5999b4bc89f89ead9466606737a79
sinha104 <sinha104@umn.edu>
2020-09-12 18:21:10 +0530

    modify page to correct homepage

---
+++ precache-manifest.8c62ba2d46dd7585395536758fdd2f61.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2069f78c95e6759f2aec7ffa21483113",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9d494566395a9d610f52",
+    "url": "./static/css/main.3aaba986.chunk.css"
+  },
+  {
+    "revision": "ca62f537a8e12af85851",
+    "url": "./static/js/2.fb9828b0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.fb9828b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d494566395a9d610f52",
+    "url": "./static/js/main.bc56cbed.chunk.js"
+  },
+  {
+    "revision": "d951622a8786f22a5d28",
+    "url": "./static/js/runtime-main.cb41e725.js"
+  },
+  {
+    "revision": "c22650c3aa49ae1b5c1ee1ae4b312792",
+    "url": "./static/media/logo_temp_1.c22650c3.png"
+  }
+]);
